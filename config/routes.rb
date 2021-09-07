8c963c94add40b92e63fe4636fb5fa6ef9cb4bd2
danieloldak <danieloldak93@gmail.com>
2021-09-07 21:01:09 +0200

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  root "home#index"
 
+  root "home#index"
+#chujowy komentarz
   resources :users, only: [:new, :create, :show]
-  
+
   resources :projects, only: [:new, :create, :show] do
     resources :tasks, only: [:new, :create, :show]
   end
-  
+
   resource :description, only: [:new, :create]
-  
+
   resources :sessions, only: [:new, :create]
   resource :sessions, only: [:destroy]
 end
