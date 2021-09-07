7b1aced1f5b7bd4ad1effd1b98f2d459570a466b
danieloldak <danieloldak93@gmail.com>
2021-09-07 21:02:31 +0200

    xxx

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TaskIt
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    #tutaj bedzie jakis setup
     config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
